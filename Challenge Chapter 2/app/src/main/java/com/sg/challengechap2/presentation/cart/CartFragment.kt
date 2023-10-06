59e009621559ff6492c9fc9982c0050a682b6dfd
sgcrusher <sandy.gustyanto13@gmail.com>
2023-10-06 16:26:12 +0700

    adding rv cart list

--- Challenge Chapter 2/app/src/main/java/com/sg/challengechap2/presentation/cart/CartFragment.kt
+++ Challenge Chapter 2/app/src/main/java/com/sg/challengechap2/presentation/cart/CartFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sg.challengechap2.R
+import com.sg.challengechap2.data.CategoryDataSource
+import com.sg.challengechap2.data.CategoryDataSourceImpl
+import com.sg.challengechap2.data.FoodDataSource
+import com.sg.challengechap2.data.FoodDataSourceImpl
+import com.sg.challengechap2.databinding.FragmentCartBinding
+import com.sg.challengechap2.databinding.FragmentFoodListBinding
 
 
 class CartFragment : Fragment() {
+    private lateinit var binding: FragmentCartBinding
 
+    private val FoodDataSource: FoodDataSource by lazy {
+        FoodDataSourceImpl()
+    }
+    private val adapter : CartListAdapter by lazy {
+        CartListAdapter(FoodDataSource)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        binding = FragmentCartBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupCartRecyclerview()
+    }
+
+    private fun setupCartRecyclerview() {
+        binding.rvCartList.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = this@CartFragment.adapter
+        }
+        adapter.setData(FoodDataSource.getFoodData())
+
     }
 }
