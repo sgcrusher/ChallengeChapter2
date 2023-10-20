ec4c9a03c8a19505e02abf65b5bba2bf8584ceb4
sgcrusher <sandy.gustyanto13@gmail.com>
2023-10-20 08:07:59 +0700

    Initial integrate API

--- Challenge Chapter 2/app/build.gradle.kts
+++ Challenge Chapter 2/app/build.gradle.kts
@@ -102,4 +102,9 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     //coil
     implementation("io.coil-kt:coil:2.4.0")
+
+    //retrofit & okhttp
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
