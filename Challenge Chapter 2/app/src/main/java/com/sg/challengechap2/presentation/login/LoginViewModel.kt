df27a14f2f5da59b21c737032764aa15c508dc1a
sgcrusher <sandy.gustyanto13@gmail.com>
2023-10-20 16:00:17 +0700

    finishing firebase login register and profile

---
+++ Challenge Chapter 2/app/src/main/java/com/sg/challengechap2/presentation/login/LoginViewModel.kt
@@ -0,0 +1,29 @@
+package com.sg.challengechap2.presentation.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sg.challengechap2.data.repository.UserRepository
+import com.sg.challengechap2.utils.ResultWrapper
+import kotlinx.coroutines.launch
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+class LoginViewModel(private val repo: UserRepository) : ViewModel() {
+
+    private val _loginResult = MutableLiveData<ResultWrapper<Boolean>>()
+
+    val loginResult: LiveData<ResultWrapper<Boolean>>
+        get() = _loginResult
+
+    fun doLogin(email: String, password: String){
+        viewModelScope.launch {
+            repo.doLogin(email, password).collect{
+                _loginResult.postValue(it)
+            }
+        }
+    }
+}
