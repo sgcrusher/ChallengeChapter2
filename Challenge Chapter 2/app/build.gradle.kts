e673e1dce5aea183724df9a082d981eeef52af96
sgcrusher <sandy.gustyanto13@gmail.com>
2023-10-20 09:38:00 +0700

    integrate firebase crashanaly, auth, finishing splashscreen and register

--- Challenge Chapter 2/app/build.gradle.kts
+++ Challenge Chapter 2/app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 
 }
 
@@ -49,6 +51,9 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.google.firebase:firebase-crashlytics:18.5.0")
+    implementation("com.google.firebase:firebase-analytics:21.4.0")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ Challenge Chapter 2/app/src/main/java/com/sg/challengechap2/presentation/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,68 @@
+package com.sg.challengechap2.presentation.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.auth.FirebaseAuth
+import com.sg.challengechap2.R
+import com.sg.challengechap2.data.network.firebase.auth.FirebaseAuthDataSource
+import com.sg.challengechap2.data.network.firebase.auth.FirebaseAuthDataSourceImpl
+import com.sg.challengechap2.data.repository.UserRepository
+import com.sg.challengechap2.data.repository.UserRepositoryImpl
+import com.sg.challengechap2.databinding.ActivitySplashScreenBinding
+import com.sg.challengechap2.presentation.login.LoginActivity
+import com.sg.challengechap2.presentation.main.MainActivity
+import com.sg.challengechap2.utils.GenericViewModelFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val viewModel : SplashScreenViewModel by viewModels{
+        GenericViewModelFactory.create(createViewModel())
+    }
+
+    private fun createViewModel(): SplashScreenViewModel{
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val dataSource: FirebaseAuthDataSource = FirebaseAuthDataSourceImpl(firebaseAuth)
+        val repository: UserRepository = UserRepositoryImpl(dataSource)
+        return SplashScreenViewModel(repository)
+    }
+
+    private val binding : ActivitySplashScreenBinding by lazy {
+        ActivitySplashScreenBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        checkIfUserLogin()
+    }
+
+    private fun checkIfUserLogin() {
+        lifecycleScope.launch {
+            delay(2000)
+            if (viewModel.isUserLoggedIn()){
+                navigateToMain()
+            } else{
+                navigateToLogin()
+            }
+        }
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
+    }
+
+    private fun navigateToMain() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
+    }
+}
