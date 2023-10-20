5a3dc9f7582c27820f985c2a7c1eafaee226f2a3
sgcrusher <sandy.gustyanto13@gmail.com>
2023-10-20 16:00:17 +0700

    finishing firebase login register and profile

---
+++ Challenge Chapter 2/app/src/main/java/com/sg/challengechap2/presentation/profile/ProfileViewModel.kt
@@ -0,0 +1,55 @@
+package com.sg.challengechap2.presentation.profile
+
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sg.challengechap2.data.repository.UserRepository
+import com.sg.challengechap2.utils.ResultWrapper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+
+class ProfileViewModel(private val repo: UserRepository) : ViewModel() {
+
+    private val _changePhotoResult = MutableLiveData<ResultWrapper<Boolean>>()
+    val changePhotoResult: LiveData<ResultWrapper<Boolean>>
+        get() = _changePhotoResult
+
+    private val _changeProfileResult = MutableLiveData<ResultWrapper<Boolean>>()
+    val changeProfileResult: LiveData<ResultWrapper<Boolean>>
+        get() = _changeProfileResult
+
+    fun getCurrentUser() = repo.getCurrentUser()
+
+    fun updateProfilePicture(photoUri: Uri?) {
+        viewModelScope.launch(Dispatchers.IO) {
+            photoUri?.let {
+                repo.updateProfile(photoUri = photoUri).collect {
+                    _changePhotoResult.postValue(it)
+                }
+            }
+        }
+    }
+
+    fun updateFullName(fullName: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.updateProfile(fullName = fullName).collect {
+                _changeProfileResult.postValue(it)
+            }
+        }
+    }
+
+    fun createChangePwdRequest() {
+        repo.sendChangePasswordRequestByEmail()
+    }
+
+    fun doLogout() {
+        repo.doLogout()
+    }
+}
