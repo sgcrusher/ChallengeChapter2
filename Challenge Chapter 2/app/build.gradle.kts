bd48a5ba90bd06f8e40abda0b5f81ac182601969
sgcrusher <sandy.gustyanto13@gmail.com>
2023-10-22 21:33:06 +0700

    fixing LoginActivity

--- Challenge Chapter 2/app/build.gradle.kts
+++ Challenge Chapter 2/app/build.gradle.kts
@@ -111,4 +111,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    debugImplementation ("com.github.chuckerteam.chucker:library:4.0.0")
+    releaseImplementation ("com.github.chuckerteam.chucker:library-no-op:4.0.0")
 }
